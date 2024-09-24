c11bcfd230e8c7201bc0be00a29ff01886ad152d
SeungGuk-Ezllabs <jsg@ezllabs.co.kr>
2024-09-25 08:56:20 +0900

    mod, 이벤트 로그 추가

--- api/handlers.go
+++ api/handlers.go
@@ -3,6 +3,7 @@ package api
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/gofiber/fiber/v3"
 	"github.com/google/uuid"
 	"image"
@@ -275,10 +276,12 @@ func addOpenEventHandler(c fiber.Ctx) error {
 
 // addSendEventHandler 전송 이벤트 핸들러
 func addSendEventHandler(c fiber.Ctx) error {
+	fmt.Println(c.Body())
 	body := new(reqAddSendEvent)
 	if err := c.Bind().JSON(&body); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
+	fmt.Println(body)
 	if body.Type != "Notification" {
 		return c.JSON(fiber.Map{})
 	}
